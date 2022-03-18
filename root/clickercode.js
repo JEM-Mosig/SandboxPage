0b9253e86a1946911b6bae6bb9b76037b55ba6b9
Johannes E. M. Mosig <jem-mosig+github@pm.me>
2022-03-18 23:18:42 +0100

    Update clickercode.js

--- root/clickercode.js
+++ root/clickercode.js
@@ -13,7 +13,7 @@ $(document).ready(function(e) {
       e.preventDefault();
       bones.push($(this).attr('class'));
       $('.selection p').html($(this).attr('class'));
-      document.getElementByID("bonelist").innerHTML = bones;
+      $(document).getElementByID("bonelist").innerHTML = bones;
     });
   
     $(document).on('click', function(e) {
